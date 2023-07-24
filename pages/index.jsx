83fe1145cf310af6f84c52afac48e023c6ad88eb
Alexis Michaltsis <a.michaltsis@gmail.com>
2023-07-24 15:04:30 +0200

    Add Next.js with example page

---
+++ pages/index.jsx
@@ -0,0 +1,166 @@
+import React from "react";
+
+// Example 1
+
+// ComponentA === ComponentB
+
+function ComponentA() {
+  return <div>Hello, world!</div>; // <- JSX, Syntical Sugar, It will be translated to React createElement function calls, NOT HTML!!!
+}
+
+// JSX from CompA gets internally translated into function of CompB
+
+// Internals, nobody uses that in their code, if their are in their right mind ;)
+
+function ComponentB() {
+  // ONLY EXAMPLE PURPOSE, DON'T USE THIS KIND OF COMPONENT IN PRODUCTION
+  return React.createElement("div", null, "Hello, world!"); // <- Function
+}
+
+// Example 2
+
+function Title() {
+  const result = 1 + 1;
+
+  return (
+    <div>
+      <h1 className="title">Title: 1 + 1 = {result}</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, itaque.
+      </p>
+    </div>
+  );
+}
+
+function TitleAsPureReactFunctions() {
+  // ONLY EXAMPLE PURPOSE, DON'T USE THIS KIND OF COMPONENT IN PRODUCTION
+  //
+  // Pure React, JSX !!! This component "ReactDoesWithTitle" (aka <ReactDoesWithTitle /> in JSX)
+  // is technically equivalent to component "Title" (aka <Title /> in JSX)
+  return React.createElement(
+    "div",
+    null,
+    ...[
+      React.createElement("h1", { className: "title" }, "Title"),
+      React.createElement(
+        "p",
+        null,
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis, itaque."
+      ),
+    ]
+  );
+}
+
+// Example 3
+
+function HelloA({ foo, bar, name }) {
+  // const { foo, bar, name } = props; // props is for example: { foo: "Foo", bar: "Bar", name: "Alice" };
+
+  // is equivalent to
+
+  // const foo = props.foo;
+  // const bar = props.bar;
+  // const name = props.name;
+
+  // return React.createElement("div", null, `Hello ${props.toWhat}`);
+  return (
+    <div>
+      Hello {foo}, {bar}, {name}!
+    </div>
+  );
+}
+
+function HelloB({ foo, bar, name }) {
+  // ONLY EXAMPLE PURPOSE, DON'T USE THIS KIND OF COMPONENT IN PRODUCTION
+  return React.createElement("div", null, `Hello ${foo}, ${bar}, ${name}`);
+}
+
+// Example 4:
+
+// ... next time: Custom Input React element where I get custom attributes and the rest in ...props.
+
+export default function ExamplePage() {
+  return (
+    <>
+      <div>
+        <h1>Example 1</h1>
+        <p>Purpose of example: ComponentA === ComponentB</p>
+        <ComponentA />
+        <ComponentB />
+      </div>
+
+      <div>
+        <h1>Example 2</h1>
+        <p>
+          Purpose of example: Title === TitleAsPureReactFunctions,
+          <br />
+          plus JSX is Javascript (name = One example)
+        </p>
+        <Title />
+        <TitleAsPureReactFunctions />
+      </div>
+
+      <div>
+        <h1>Example 3</h1>
+        <HelloA foo="Foo" bar="Bar" name="Alice" />
+        <HelloA foo="Foo" bar="Bar" name="Bob" />
+        <HelloB foo="Foo" bar="Bar" name="Charlie" />
+      </div>
+
+      {/* Entry point in your RLI Django-React project */}
+      <div id="root"></div>
+    </>
+  );
+}
+
+// NOTES:
+
+// React has two ways of creating componens, class-based and function-based.
+// Function-based is the modern, recommended way!!!
+
+// JSX = "J"ava"S"cript an "X"ML
+
+//
+// Every {} in JSX is going into JavaScript-Land. In there you can use normal JS assignments.
+// Examples:
+//
+// - a.): 1 + 1
+// - b.): "Bob"
+// - c.): () => console.log("foo")
+//
+
+// React is nothing else than Client-Side JS which inhabits one element of your DOM,
+// Most of the time a div element wrapped by the HTML body-tag.
+//
+// Please note:
+//
+// - But there it wants to rule complelty alone !!!
+// - No other JS is allowed in there, no JQuery, no custom JS or other libraries.
+//
+// Reason:
+//
+// React does NOT operate in the real "DOM", React operates in the so called "Virtual DOM".
+// In the "Virtual DOM" world, it produces one "Virtual DOM" (current) and on changes a second "Virtual DOM" (next)
+// and does a difference calculation (aka diff calculation) between those (diff: current <-> next).
+// And ONLY injects the differences (surgically) in the real "DOM" (changedElement.innerHTML = "I changed :D").
+//
+// In Principle:
+//
+// Real-DOM (looks at it once on startup) -> Virtual-DOM (current) -> Virtual-DOM` (aka changed, next DOM) -> diff of both VDOMs ->
+// inject into Real-DOM -> on success, "next" VDOM becomes "current", and cylce repeats.
+//
+// It does this on EVERY change!!! If you have many changes and or frequent changes,
+// this will slow down React to a hold, because of to many changes.
+//
+// Thumb Rules:
+//
+// 1.)  Creating a real new DOM in a browser (like Chrome) is very expensive. Expensive means,
+//      its taking CPU cycles (aka calculation power) and therefore time.
+//
+// 2.)  Injecting only changes via "elemnet.innerHTML" is much cheaper, thats why React does this.
+//
+// 3.)  BUt it has a Virtual DOM diffing cost, thats why "Virtual DOM diffing" like React does it
+//      is a dumb but good enough solution (most of the time)
+//
+// 4.)  Better and faster solution always diff and inject on the real "DOM".
+//      For example: Svelte, SolidJS, ...
