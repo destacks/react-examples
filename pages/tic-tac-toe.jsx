d571e7ded0d7be6bff8811ae0410fb7cfebda73b
Bryan Lancien <bryanlancien.ui@gmail.com>
2023-08-11 09:56:42 +0100

    build the board

--- pages/tic-tac-toe.jsx
+++ pages/tic-tac-toe.jsx
@@ -1,6 +1,24 @@
 import React, { useState } from "react";
 import styles from '@/styles/Tic-tac-toe.module.css'
 
-export default function Square() {
-  return <button className={styles.square}>X</button>;
+export default function Board() {
+  return (
+    <>
+      <div className={styles.boardRow}>
+        <button className={styles.square}>X</button>
+        <button className={styles.square}>X</button>
+        <button className={styles.square}>X</button>
+      </div>
+      <div className={styles.boardRow}>
+        <button className={styles.square}>X</button>
+        <button className={styles.square}>X</button>
+        <button className={styles.square}>X</button>
+      </div>
+      <div className={styles.boardRow}>
+        <button className={styles.square}>X</button>
+        <button className={styles.square}>X</button>
+        <button className={styles.square}>X</button>
+      </div>
+    </>
+  )
 }
