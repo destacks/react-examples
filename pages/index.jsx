55e3562f92a958ce8bea9aa53b212f84fe0e18ca
Bryan Lancien <bryanlancien.ui@gmail.com>
2023-08-11 09:35:00 +0100

    start tic tac toe game

--- pages/index.jsx
+++ pages/index.jsx
@@ -12,12 +12,20 @@ const Home = () => (
           Examples 01 - Jul 24, 2023
         </Link>
       </li>
-      <li>
+      <li className="my-4">
         <Link
           href="/examples-02"
           className="my-8 text-lg font-medium hover:underline underline-offset-2 decoration-2 hover:decoration-pink-400"
         >
           Examples 02 - Aug 8, 2023
+        </Link>
+      </li>
+      <li className="my-4">
+        <Link
+          href="/tic-tac-toe"
+          className="my-8 text-lg font-medium hover:underline underline-offset-2 decoration-2 hover:decoration-pink-400"
+        >
+          Tic tac toe tutorial
         </Link>
       </li>
     </ul>
