72fcc14cb0601c9c695165cd90b3c934c3a986cf
Bryan Lancien <bryanlancien.ui@gmail.com>
2023-08-11 09:50:40 +0100

    add css file

--- pages/tic-tac-toe.jsx
+++ pages/tic-tac-toe.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
+import styles from '@/styles/Tic-tac-toe.module.css'
 
 export default function Square() {
-  return <button className="square">X</button>;
+  return <button className={styles.square}>X</button>;
 }
