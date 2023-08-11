99af5e243bf87544a9b397e4b10c8d7cd4f0a6d6
Bryan Lancien <bryanlancien.ui@gmail.com>
2023-08-11 10:09:45 +0100

    passing data through props

--- pages/tic-tac-toe.jsx
+++ pages/tic-tac-toe.jsx
@@ -1,23 +1,27 @@
 import React, { useState } from "react";
 import styles from '@/styles/Tic-tac-toe.module.css'
 
+function Square({ value }) {
+  return <button className={styles.square}>{value}</button>
+}
+
 export default function Board() {
   return (
     <>
       <div className={styles.boardRow}>
-        <button className={styles.square}>X</button>
-        <button className={styles.square}>X</button>
-        <button className={styles.square}>X</button>
+        <Square value="1" />
+        <Square value="2" />
+        <Square value="3" />
       </div>
       <div className={styles.boardRow}>
-        <button className={styles.square}>X</button>
-        <button className={styles.square}>X</button>
-        <button className={styles.square}>X</button>
+        <Square value="4" />
+        <Square value="5" />
+        <Square value="6" />
       </div>
       <div className={styles.boardRow}>
-        <button className={styles.square}>X</button>
-        <button className={styles.square}>X</button>
-        <button className={styles.square}>X</button>
+        <Square value="7" />
+        <Square value="8" />
+        <Square value="9" />
       </div>
     </>
   )
